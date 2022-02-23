9e1ca991548be433dc6df694a998c02b3229e588
andrii.yanko <andrii.yanko@globallogic.com>
2022-02-23 12:20:23 +0200

    Initial commit

---
+++ src/main/java/com/example/andy/wildflydb/WildflyDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.andy.wildflydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildflyDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WildflyDbApplication.class, args);
+    }
+
+}
